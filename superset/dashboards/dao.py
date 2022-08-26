cfec96f44ed3ed10fc8858648e3003e13b2469ed
Christy George <christy@teamstreamz.com>
2022-08-26 17:39:57 +0530

    Changes to fix long line

--- superset/dashboards/dao.py
+++ superset/dashboards/dao.py
@@ -303,6 +303,7 @@ class DashboardDAO(BaseDAO):
             if tobject.tag.type == TagTypes.custom
         ]
         return reduce(
-            # pylint: disable-next=line-too-long
-            lambda d, dt: d.setdefault(dt[0], []).append(dt[1]) or d, dashboard_tags, {}  # type: ignore
+            lambda d, dt: d.setdefault(dt[0], []).append(dt[1]) or d,
+            dashboard_tags,
+            {},  # type: ignore
         )
